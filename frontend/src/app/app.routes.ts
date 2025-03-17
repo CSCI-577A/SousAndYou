6a8e4d285fa111117211f24c92a51facf9a8b7d9
aprildawoud <42796721+aprildawoud@users.noreply.github.com>
2025-03-16 16:00:16 -1000

    added navbar and about us page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,41 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Home' },
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About',
+  },
+  // {
+  //   path: 'how-it-works',
+  //   loadComponent: () => import('./how-it-works/how-it-works.component').then(m => m.HowItWorksComponent),
+  //   title: 'How It Works'
+  // },
+  // {
+  //   path: 'shopping-list',
+  //   loadComponent: () => import('./shopping-list/shopping-list.component').then(m => m.ShoppingListComponent),
+  //   title: 'Shopping List'
+  // },
+  // {
+  //   path: 'about',
+  //   loadComponent: () => import('./about/about.component').then(m => m.AboutComponent),
+  //   title: 'About Us'
+  // },
+  // {
+  //   path: 'results',
+  //   loadComponent: () => import('./results/results.component').then(m => m.ResultsComponent),
+  //   title: 'Search Results'
+  // },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
