42c11c464d14a1a2360ae078b6f427e0583d0fcd
HapiPeas <94872538+HapiPeas@users.noreply.github.com>
2025-04-19 20:09:29 -0400

    test new frontend deployment

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -95,6 +95,7 @@ export class HomeComponent {
         this.searchResults = response.results;
         console.log('Search Results:', this.searchResults);
       });
+    }
   }
 
 
